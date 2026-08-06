9ed4d0d1626bc8b3a044ca4192aa38db5ca06a59
Aman Prajapati <127750213+Aman2228@users.noreply.github.com>
2026-08-06 22:50:27 +0530

    Update ocs_logic.py

--- ocs_logic.py
+++ ocs_logic.py
@@ -136,10 +136,7 @@ def roundcube_compose_session(password):
             "Roundcube compose ID was not found."
         )
 
-
-    with open("compose.html", "w", encoding="utf-8") as f:
-        f.write(compose_page.text)
-    print("compose.html written")
+    print(compose_page.text)
     
     session.roundcube_compose_id = compose_id_match.group(1)
     return session, token
